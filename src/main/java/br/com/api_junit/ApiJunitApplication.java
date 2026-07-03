d6354b91b84ce3c1fd865d9218ac7ddf85b7c3ec
Rafael Fujii <rafael987.456@gmail.com>
2026-07-03 10:30:09 -0300

    first-commit

---
+++ src/main/java/br/com/api_junit/ApiJunitApplication.java
@@ -0,0 +1,13 @@
+package br.com.api_junit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiJunitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiJunitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/api_junit/ApiJunitApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api_junit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiJunitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
