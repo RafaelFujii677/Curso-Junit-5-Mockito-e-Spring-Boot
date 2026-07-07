29bc5468ce9e22c98c2d66af3d9c5dae0aee542a
Rafael Fujii <rafael987.456@gmail.com>
2026-07-07 13:03:54 -0300

    Aula 23 - Teste UserResource update

--- src/test/java/br/com/api_junit/resources/UserResourceTest.java
+++ src/test/java/br/com/api_junit/resources/UserResourceTest.java
@@ -100,6 +100,24 @@ public class UserResourceTest {
 		assertNotNull(response.getHeaders().get("Location"));
 	}
 
+	@Test
+	void whenUpdateThenReturnSuccess() {
+		when(service.update(any())).thenReturn(user);
+		when(mapper.map(any(), any())).thenReturn(userDTO);
+
+		ResponseEntity<UserDTO> response = resource.update(ID, userDTO);
+
+		assertNotNull(response);
+		assertNotNull(response.getBody());
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+		assertEquals(ResponseEntity.class, response.getClass());
+		assertEquals(UserDTO.class, response.getBody().getClass());
+
+		assertEquals(ID, response.getBody().getId());
+		assertEquals(NAME, response.getBody().getName());
+		assertEquals(EMAIL, response.getBody().getEmail());
+	}
+
 	private void startUser() {
 		user = new User(ID, NAME, EMAIL, PASSWORD);
 		userDTO = new UserDTO(ID, NAME, EMAIL, PASSWORD);
