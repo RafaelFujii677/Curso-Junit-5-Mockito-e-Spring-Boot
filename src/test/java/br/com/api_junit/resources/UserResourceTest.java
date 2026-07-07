616d3176fe6f3c77128690b204b67e296fd4926b
Rafael Fujii <rafael987.456@gmail.com>
2026-07-07 13:38:39 -0300

    Aula 27 - Bugfix

--- src/test/java/br/com/api_junit/resources/UserResourceTest.java
+++ src/test/java/br/com/api_junit/resources/UserResourceTest.java
@@ -45,8 +45,8 @@ public class UserResourceTest {
 	@Mock
 	private ModelMapper mapper;
 
-	private User user;
-	private UserDTO userDTO;
+	private User user = new User();
+	private UserDTO userDTO = new UserDTO();
 
 	@BeforeEach
 	void setUp() {
